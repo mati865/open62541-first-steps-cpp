9f451b9b85ee054beec3934c15dba33ad0cc1880
Wojciech Bartnik <yisonPylkita@gmail.com>
2021-08-05 10:15:17 +0200

    Now benchmark works as expected ^_^

--- spike-client.cpp
+++ spike-client.cpp
@@ -12,34 +12,35 @@
 
 using namespace std::string_literals;
 
-constexpr auto VAR_COUNT = 1;
+constexpr auto VAR_COUNT = 1000;
 
 static long long
-current_timestamp(void) {
+current_timestamp_in_us(void) {
     struct timeval te;
     gettimeofday(&te, NULL);
     long long microseconds = te.tv_sec * 1000 * 1000 + te.tv_usec;
     return microseconds;
 }
 
-char * leak_memory(std::string src) {
-    auto ptr = new char[src.size()]{};
+char *leak_memory(std::string src)
+{
+    auto ptr = new char[src.size() + 1]{};
     mempcpy(ptr, src.c_str(), src.size());
     return ptr;
 }
 
 std::string get_var_name(int index) {
-    return "server_var_"s + std::to_string(index);
+    return "servervar"s + std::to_string(index);
 }
 
 std::string get_node_id_name(int index) {
-    return "node_id_"s + std::to_string(index);
+    return "nodeid"s + std::to_string(index);
 }
 
 std::vector<UA_NodeId> NODE_IDS{};
 void init_node_ids() {
     for (auto i = 0; i < VAR_COUNT; i++) {
-        NODE_IDS.push_back(UA_NODEID_STRING(0, leak_memory(get_var_name(i))));
+        NODE_IDS.push_back(UA_NODEID_STRING(1, leak_memory(get_node_id_name(i))));
     }
 }
 
@@ -109,21 +110,32 @@ main(void) {
         return (int)retval;
     }
 
-    constexpr auto TEN_SECONDS = 10'000'000;
-    const long long EXECUTION_TIME = TEN_SECONDS;
-    long long one_sec_start = current_timestamp();
+    constexpr auto ONE_SECOND = 1'000'000;
+    constexpr auto pumpFuelLevelONDS = 10 * ONE_SECOND;
+    const long long EXECUTION_TIME = ONE_SECOND;
+    long long requests_start = current_timestamp_in_us();
     long long processed_requests = 0;
     while(true) {
-        if((current_timestamp() - one_sec_start) > EXECUTION_TIME) {
+        if((current_timestamp_in_us() - requests_start) > EXECUTION_TIME) {
             break;
         }
+        long long before_ts = current_timestamp_in_us();
 
-        long long before_ts = current_timestamp();
-        /* Read the value attribute of the node. UA_Client_readValueAttribute is a
-         * wrapper for the raw read service available as UA_Client_Service_read. */
-        UA_Variant value; /* Variants can hold scalar values and arrays of any type */
+        //  /* Read attribute */
+        // UA_Int32 value = 0;
+        // printf("\nReading the value of node (1, \"nodeid35\"):\n");
+        // UA_Variant *val = UA_Variant_new();
+        // retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(1, "nodeid35"), val);
+        // if(retval == UA_STATUSCODE_GOOD && UA_Variant_isScalar(val) &&
+        // val->type == &UA_TYPES[UA_TYPES_INT32]) {
+        //         // value = *(UA_Int32*)val->data;
+        //         printf("the value is: %i\n", value);
+        // }
+        // UA_Variant_delete(val);
+
+
+        UA_Variant value;
         UA_Variant_init(&value);
-
         retval = read_variables(client, &value);
 
         // if(retval == UA_STATUSCODE_GOOD &&
@@ -137,16 +149,17 @@ main(void) {
         //                 dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
         // }
 
-        UA_Variant_clear(&value);
+        if(retval == UA_STATUSCODE_GOOD) {
+            processed_requests += 1;
+            // UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+            //             "Array len: %ld\n", value.arrayLength);
+        }
 
         // UA_sleep_ms(100);
 
-        printf("Request took about %lldus\n", current_timestamp() - before_ts);
+        printf("Request took %lldus\n", current_timestamp_in_us() - before_ts);
     }
-    printf("Processed %lld requests in %lldus\n", processed_requests, EXECUTION_TIME);
-    printf("Speed: %lld req/second\n",
-           processed_requests / (EXECUTION_TIME / 1000));
-
+    printf("Processed %lld requests and %lld Data Points in %lldus\n", processed_requests, processed_requests * VAR_COUNT, EXECUTION_TIME);
     /* Clean up */
     UA_Client_delete(client); /* Disconnects the client internally */
     return EXIT_SUCCESS;
--- spike-server.cpp
+++ spike-server.cpp
@@ -9,65 +9,88 @@
 #include <vector>
 #include <string>
 
-
 using namespace std::string_literals;
 
-constexpr auto VAR_COUNT = 1;
+constexpr auto VAR_COUNT = 1000;
 
-static const UA_NodeId baseDataVariableType = {
-    0, UA_NODEIDTYPE_NUMERIC, {UA_NS0ID_BASEDATAVARIABLETYPE}};
-
-char * leak_memory(std::string src) {
-    auto ptr = new char[src.size()]{};
+char *leak_memory(std::string src) {
+    auto ptr = new char[src.size() + 1]{};
     mempcpy(ptr, src.c_str(), src.size());
     return ptr;
 }
 
 std::string get_var_name(int index) {
-    return "server_var_"s + std::to_string(index);
+    return "servervar"s + std::to_string(index);
 }
 
 std::string get_node_id_name(int index) {
-    return "node_id_"s + std::to_string(index);
+    return "nodeid"s + std::to_string(index);
 }
 
 static void
 addVariable(UA_Server *server, int index) {
     /* add a static variable node to the server */
-    UA_VariableAttributes myVar = UA_VariableAttributes_default;
-    myVar.displayName = UA_LOCALIZEDTEXT("en-US", leak_memory(get_var_name(index)));
-    myVar.accessLevel = UA_ACCESSLEVELMASK_READ;
-    myVar.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
-    myVar.valueRank = UA_VALUERANK_SCALAR;
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
     UA_Int32 myInteger = index;
-    UA_Variant_setScalar(&myVar.value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
-    const UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, leak_memory(get_var_name(index)));
-    const UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, leak_memory(get_node_id_name(index)));
+    UA_Variant_setScalar(&attr.value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+    attr.displayName = UA_LOCALIZEDTEXT("en-US", leak_memory(get_var_name(index)));
+    attr.description = UA_LOCALIZEDTEXT("en-US", leak_memory(get_var_name(index)));
+    attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+
+    const UA_NodeId currentNodeId = UA_NODEID_STRING(1, leak_memory(get_node_id_name(index)));
+    const UA_QualifiedName currentName = UA_QUALIFIEDNAME(1, leak_memory(get_var_name(index)));
     UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
     UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_NodeId variableTypeNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE);
+    UA_Server_addVariableNode(server, currentNodeId, parentNodeId,
+                              parentReferenceNodeId, currentName, variableTypeNodeId,
+                              attr, NULL, NULL);
+}
+
+static void
+addVariableFromExample(UA_Server *server) {
+    /* Define the attribute of the myInteger variable node */
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    UA_Int32 myInteger = 95;
+    UA_Variant_setScalar(&attr.value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+    attr.description = UA_LOCALIZEDTEXT("en-US","pump fuel level");
+    attr.displayName = UA_LOCALIZEDTEXT("en-US","pump fuel level");
+    attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+
+    /* Add the variable node to the information model */
+    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, leak_memory(get_node_id_name(35)));
+    UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, "pump fuel level");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_NodeId variableTypeNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE);
     UA_Server_addVariableNode(server, myIntegerNodeId, parentNodeId,
-                              parentReferenceNodeId, myIntegerName, baseDataVariableType,
-                              myVar, NULL, NULL);
+                              parentReferenceNodeId, myIntegerName, variableTypeNodeId,
+                              attr, NULL, NULL);
 }
 
 static volatile UA_Boolean running = true;
 static void
-stopHandler(int sig) {
+stopHandler(int sig)
+{
     UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
     running = false;
 }
 
-int
-main(void) {
+int main(void)
+{
     signal(SIGINT, stopHandler);
     signal(SIGTERM, stopHandler);
 
     UA_Server *server = UA_Server_new();
     UA_ServerConfig_setDefault(UA_Server_getConfig(server));
 
-    for(int i = 0; i < VAR_COUNT; i++) {
+    for (int i = 0; i < VAR_COUNT; i++) {
         addVariable(server, i);
     }
+
+    // addVariableFromExample(server);
 
     UA_StatusCode retval = UA_Server_run(server, &running);
 
